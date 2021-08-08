a21d003fd2ef2cc38208aecaf47e44beafd047e8
Kirill Rozov <krl.rozov@gmail.com>
2021-08-08 22:30:35 +0300

    Add documentation to ApiAvailabilityServices

--- core/core-common/src/main/java/dev/androidbroadcast/ums/core/ApiAvailabilityServices.kt
+++ core/core-common/src/main/java/dev/androidbroadcast/ums/core/ApiAvailabilityServices.kt
@@ -2,12 +2,26 @@ package dev.androidbroadcast.ums.core
 
 import android.content.Context
 
+/**
+ * Abstraction on ApiAvailability APIs of mobile services
+ */
 public interface ApiAvailabilityServices {
 
+    /**
+     * Unique string that identify implementation of [ApiAvailabilityServices]
+     */
     public val serviceId: String
 
+    /**
+     * Check is services present on device. Maybe require update, or isn't working, but exists
+     */
     public fun isServicesAvailable(context: Context): Boolean
 
+    /**
+     * Package name of store with what the services is connected. As example,
+     * for Google Play Services it will be Google Play with package name 'com.android.vending'.
+     *
+     * Can be null in case when services are independent of any app store.
+     */
     public val storePackageName: String?
-        get() = null
 }
